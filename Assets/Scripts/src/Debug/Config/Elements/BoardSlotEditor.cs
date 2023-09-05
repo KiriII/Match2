b6fd160ece9276870fa2602ee956d17c62aefb85
Kirill Kulagin <kirill.k@ugo.company>
2023-09-05 10:54:47 +0300

    fixes for level config editor

--- Assets/Scripts/src/Debug/Config/Elements/BoardSlotEditor.cs
+++ Assets/Scripts/src/Debug/Config/Elements/BoardSlotEditor.cs
@@ -21,14 +21,13 @@ namespace Match3Debug.Configs
                 {
                     var slotSkin = level.slots[i, j].CanHoldCell ? GUI.skin.button : GUI.skin.textArea;
                     if (!level.slots[i, j].IsActive) slotSkin = GUI.skin.box;
-                    //slotSkin.normal.background = Texture2D.blackTexture;
                     EditorGUILayout.BeginVertical(slotSkin, GUILayout.Width(70), GUILayout.Height(60));
                     EditorGUILayout.Space();
                     if (GUILayout.Button($"edit", GUI.skin.button))
                     {
                         var window = new BoardSlotWindow(level.slots[i,j], level.ID);
                     }
-                    
+
                     if (level.slots[i, j].CanHoldCell && level.slots[i, j].IsActive)
                     {
                         if (level.slots[i, j].IsBlocked)
@@ -55,6 +54,7 @@ namespace Match3Debug.Configs
                                     EditorGUILayout.ColorField(Color.yellow);
                                     break;
                                 default:
+                                    EditorGUILayout.ColorField(Color.gray);
                                     break;
                             }
                         }
@@ -82,7 +82,8 @@ namespace Match3Debug.Configs
             x = slot.Coordinate.x;
             y = slot.Coordinate.y;
 
-            var window = GetWindow(typeof(BoardSlotWindow), false, "cell");
+            var window = GetWindow(typeof(BoardSlotWindow));
+            window.titleContent = new GUIContent($"cell pos {x} {y}");
             window.Show();
         }
 
